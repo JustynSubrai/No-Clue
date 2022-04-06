54b8440fbaf58dbb312833a9dc0ab3d9d3b1d1c7
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-04-06 12:19:28 -0700

    worked on post.js

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,24 +1,33 @@
-const source = document.getElementById('card-section').innerHTML;
-// let cardEl = document.getElementById('card-section');
 let nameEl = document.getElementById('name-input').value;
 let priceEl = document.getElementById('price-input').value;
 let infoEl = document.getElementById('info-input').value;
-let imgEl = document.getElementById('img-input').value;
-// let userEl = document.getElementById('username').value;
-let cardEl = Handlebars.compile(source);
-
 
 async function post(event) {
-    console.log('post btn working')
     event.preventDefault();
-    const response = await fetch('/post', {
+    let data = {
+        context: [
+            {
+                info: `<div id='item-card' class='card col-2.5 m-3' style='width: 18rem;'>
+                <div class='card-body'>
+                <h5 class='card-title' id='item-name'>{{nameEl}}</h5>
+                <img class='card-img-top' src='.../100px180/?text=Image cap' alt='Card image cap'>{{}}</img>
+                </div>
+                
+                <ul class='list-group list-group-flush'>
+                <li class='list-group-item' id='item-price'>{{priceEl}}</li>
+                <li class='list-group-item' id='item-description'>{{infoEl}}</li>
+                </ul>
+                </div>
+                `}
+        ]
+    };
+
+    const response = await fetch('/api/items', {
         method: 'post',
         body: JSON.stringify({
             nameEl,
             priceEl,
             infoEl,
-            imgEl
-            // userEl
         }),
         headers: { 'Content-Type': 'application/json' }
     });
@@ -29,21 +38,15 @@ async function post(event) {
         alert(response.statusText);
     }
 
-    const content = `
-        <div id='item-card' class="card col-2.5 m-3" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title" id="item-name">${nameEl}</h5>
-            <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-        </div>
-        
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item" id="item-price">${priceEl}</li>
-            <li class="list-group-item" id="item-description">${infoEl}</li>
-        </ul>
-        </div>
-        `;
-let html = cardEl(content)
+    let tempEl = Handlebars.compile(document.querySelector('#template'));
+    let cardEl = tempEl(data, {
+        noEscape: true
+    });
+
+    document.querySelector('#card-section').innerHTML = cardEl;
+
+    console.log('=========POST BTN WORKING==========')
 };
 
 
-document.querySelector('#post-item').addEventListener('click', post);
+document.getElementById('post-item').addEventListener('click', post);
