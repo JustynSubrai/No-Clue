a9a67dda27ace1c7a457fc0246924d697f60458b
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-04-05 18:46:13 -0700

    created post button logic

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,19 +1,7 @@
 async function createCardHandler(event) {
     event.preventDefault();
-
-    const response = await fetch('/post', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        document.location.replace('/homepage/');
-    } else {
-        alert(response.statusText);
-    }
-
+        document.location.replace('/post');
+        console.log('========postbtn WORKINGGGGGG==========')
 }
 
 
