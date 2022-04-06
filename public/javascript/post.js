bb8cd3dae09d5ff3e07329a5c1252e9661564805
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-04-06 16:18:14 -0700

    got name data to work post.js

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -1,44 +1,51 @@
-let nameEl = document.getElementById('name-input').value;
-let priceEl = document.getElementById('price-input').value;
-let infoEl = document.getElementById('info-input').value;
 async function post(event) {
     event.preventDefault();
-    let data = {
+    
+    let nameEl = document.getElementById('name-input').value;
+    let priceEl = document.getElementById('price-input').value;
+    let infoEl = document.getElementById('info-input').value;
+    
+    let content = {
         context: [
             {
                 info: `<div id='item-card' class='card col-2.5 m-3' style='width: 18rem;'>
                 <div class='card-body'>
-                <h5 class='card-title' id='item-name'>{{nameEl}}</h5>
-                <img class='card-img-top' src='.../100px180/?text=Image cap' alt='Card image cap'>{{}}</img>
+                <h5 class='card-title' id='item-name'>${nameEl}</h5>
+                <img class='card-img-top' src='.../100px180/?text=Image cap' alt='Card image cap'>
                 </div>
-                
+
                 <ul class='list-group list-group-flush'>
-                <li class='list-group-item' id='item-price'>{{priceEl}}</li>
-                <li class='list-group-item' id='item-description'>{{infoEl}}</li>
+                <li class='list-group-item' id='item-price'>${priceEl}</li>
+                <li class='list-group-item' id='item-description'>${infoEl}</li>
                 </ul>
                 </div>
                 `}
         ]
     };
 
-    const response = await fetch('/api/items', {
-        method: 'post',
-        body: JSON.stringify({
-            nameEl,
-            priceEl,
-            infoEl,
-        }),
-        headers: { 'Content-Type': 'application/json' }
-    });
+    const name = nameEl.trim();
+    const price = priceEl;
+    const description = infoEl.trim();
 
-    if (response.ok) {
-        document.location.replace('/homepage/');
-    } else {
-        alert(response.statusText);
+    if (name && price && description) {
+        const response = await fetch('/api/items', {
+            method: 'post',
+            body: JSON.stringify({
+                name,
+                price,
+                description
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/homepage/');
+        } else {
+            alert(response.statusText);
+        }
     }
-
     let tempEl = Handlebars.compile(document.querySelector('#template'));
-    let cardEl = tempEl(data, {
+    let cardEl = tempEl(content, {
         noEscape: true
     });
 
@@ -46,6 +53,5 @@ async function post(event) {
 
     console.log('=========POST BTN WORKING==========')
 };
-
 
 document.getElementById('post-item').addEventListener('click', post);
--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,39 +1,7 @@
 const searchWrapper = document.querySelector("#searchbar");
 const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector(".autocom-box");
-const icon = searchWrapper.querySelector('#icon');
 let linkTag = searchWrapper.querySelector('a');
 let webLink;
-
-
-inputBox.onkeyup = (e) => {
-  let userData = e.target.value; //user enetered data
-  let emptyArray = [];
-  if (userData) {
-    icon.onclick = () => {
-      webLink = `api/items/${userData}`;
-      linkTag.setAttribute("href", webLink);
-      linkTag.click();
-    }
-    emptyArray = suggestions.filter((data) => {
-      //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-    });
-    emptyArray = emptyArray.map((data) => {
-      // passing return data inside li tag
-      return data = `<li>${data}</li>`;
-    });
-    searchWrapper.classList.add("active"); //show autocomplete box
-    showSuggestions(emptyArray);
-    let allList = suggBox.querySelectorAll("li");
-    for (let i = 0; i < allList.length; i++) {
-      //adding onclick attribute in all li tag
-      allList[i].setAttribute("onclick", "select(this)");
-    }
-  } else {
-    searchWrapper.classList.remove("active"); //hide autocomplete box
-  }
-}
 
 
 function select(element){
@@ -46,16 +14,3 @@ function select(element){
   }
   searchWrapper.classList.remove("active");
 };
-
-function showSuggestions(list){
-  let listData;
-  if(!list.length){
-      userValue = inputBox.value;
-      listData = `<li>${userValue}</li>`;
-  }else{
-    listData = list.join('');
-  }
-  suggBox.innerHTML = listData;
-}
-
-
