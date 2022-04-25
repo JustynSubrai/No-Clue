c7d1748b58627ce16af55ed7b5340b7150c9a338
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-04-25 15:55:32 -0700

    working on cloudinary

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
+const { format } = require('path');
+const res = require('express/lib/response');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
